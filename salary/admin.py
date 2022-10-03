78d44f2d339774e17b238738bfaf14898dacb4e6
kinganuoluwa <adekogbonking03@gmail.com>
2022-10-03 22:46:33 +0100

    update employee module with deductions and allowance

--- salary/admin.py
+++ salary/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Salary
+from .models import Allowance, Deduction, Salary
 
 
+admin.site.register(Allowance)
+admin.site.register(Deduction)
 admin.site.register(Salary)
--- salary/models.py
+++ salary/models.py
@@ -5,6 +5,17 @@ from employees.models import Employee
 class Salary(models.Model):
     employee = models.OneToOneField(Employee, on_delete=models.CASCADE)
     basic_salary = models.FloatField()
-    
+
     def __str__(self):
         return f"{self.employee.first_name} {self.employee.last_name}'s Salary"
+
+class Deduction(models.Model):
+    employee  = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    fine = models.FloatField()
+    tax = models.FloatField()
+    loan = models.FloatField()
+
+class Allowance(models.Model):
+    employee  = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    feeding = models.FloatField()
+    housing = models.FloatField()
